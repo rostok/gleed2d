3737f41b2a960698f4781fc19a12f2bed19dfdab
rostok <rostok@ATIVAN>
2018-01-12 21:01:42 +0100

    update to VS 2017

--- gleed2d/Properties/AssemblyInfo.cs
+++ gleed2d/Properties/AssemblyInfo.cs
@@ -30,10 +30,11 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.16")]
+[assembly: AssemblyVersion("1.3.0.17")]
 
 /*
  * rostok's changes
+ * 1.3.0.17 - update to VS 2017
  * 1.3.0.16 - delete path points with Shift Click
  * 1.3.0.15 - increase indexes in names while copying
  * 1.3.0.14 - copy / paste objects between Glee instances
